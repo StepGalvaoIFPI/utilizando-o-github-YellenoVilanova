8d5e06a9bb194b34a906496719e3de3a03e54d6f
YellenoVilanova <yellenno.vilanova@gmail.com>
2025-04-08 21:07:54 -0300

    Update q2.py
    Resposta questão 02

--- respostas/q2.py
+++ respostas/q2.py
@@ -1,3 +1,4 @@
 # Crie um programa que exiba as seguintes informações, cada uma em uma linha.
 print("Yelleno")
 print("Yelleno, Licenciatura em Computação")
+print("Yelleno, Licenciatura em Computação, IFPI")
